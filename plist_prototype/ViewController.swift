c1827f97b4efd6b115f523b9b39ea9cb22a68bbb
jverbosky <jverbosk@gmail.com>
2017-05-01 15:47:25 -0400

    refactored evaluatePlist() to accept key and value in prep for handling push notification enablement status

--- plist_prototype/ViewController.swift
+++ plist_prototype/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
 
     let dataPlistName = "Login"
     let usernameKey = "username"  // plist username key
+    var usernameValue:String = ""  // plist username value to post to Sinatra app
+    let pneStatusKey = "pneStatus"  // push notification enablement status key
+    var pneStatusValue:String = ""  // push notification enablement status value to post to Sinatra app
     
     @IBOutlet weak var inputField: UITextField!
     @IBOutlet weak var statusUpdate: UILabel!
@@ -28,28 +31,27 @@ class ViewController: UIViewController {
         // readPlist(usernameKey)
         
     }
-    
+
     // Function to collect username from input field
     @IBAction func submitButton(_ sender: UIButton) {
         
         if inputField.text != "" {
-            evaluatePlist(inputField.text!)
+            evaluatePlist(usernameKey, inputField.text!)
             statusUpdate.text = "Thank you \(inputField.text!)!"
         } else {
             statusUpdate.text = "Username not detected - please try again!"
         }
     }
     
-    
     // Function to determine if plist is already populated
-    func evaluatePlist(_ usernameValue:String) {
+    func evaluatePlist(_ key:String, _ value:String) {
         
         // Run function to add key/value pairs if plist empty, otherwise run function to update values
-        SwiftyPlistManager.shared.getValue(for: usernameKey, fromPlistWithName: dataPlistName) { (result, err) in
+        SwiftyPlistManager.shared.getValue(for: key, fromPlistWithName: dataPlistName) { (result, err) in
             if err != nil {
-                populatePlist(usernameKey, usernameValue)
+                populatePlist(key, value)
             } else {
-                updatePlist(usernameKey, usernameValue)
+                updatePlist(key, value)
             }
         }
     }
@@ -88,6 +90,68 @@ class ViewController: UIViewController {
             }
         }
     }
+
+    
+    
+//    // Function to collect username from input field
+//    @IBAction func submitButton(_ sender: UIButton) {
+//        
+//        if inputField.text != "" {
+//            evaluatePlist(inputField.text!)
+//            statusUpdate.text = "Thank you \(inputField.text!)!"
+//        } else {
+//            statusUpdate.text = "Username not detected - please try again!"
+//        }
+//    }
+//    
+//    
+//    // Function to determine if plist is already populated
+//    func evaluatePlist(_ usernameValue:String) {
+//        
+//        // Run function to add key/value pairs if plist empty, otherwise run function to update values
+//        SwiftyPlistManager.shared.getValue(for: usernameKey, fromPlistWithName: dataPlistName) { (result, err) in
+//            if err != nil {
+//                populatePlist(usernameKey, usernameValue)
+//            } else {
+//                updatePlist(usernameKey, usernameValue)
+//            }
+//        }
+//    }
+//    
+//    // Function to populate empty plist file with specified key/value pair
+//    func populatePlist(_ key:String, _ value:String) {
+//        SwiftyPlistManager.shared.addNew(value, key: key, toPlistWithName: dataPlistName) { (err) in
+//            if err == nil {
+//                print("-------------> Value '\(value)' successfully added at Key '\(key)' into '\(dataPlistName).plist'")
+//            }
+//        }
+//    }
+//    
+//    // Function to update specified key/value pair in plist file
+//    func updatePlist(_ key:String, _ value:String) {
+//        SwiftyPlistManager.shared.save(value, forKey: key, toPlistWithName: dataPlistName) { (err) in
+//            if err == nil {
+//                print("------------------->  Value '\(value)' successfully saved at Key '\(key)' into '\(dataPlistName).plist'")
+//            }
+//        }
+//    }
+//    
+//    // Function to read key/value pairs out of plist
+//    func readPlist(_ key:Any) {
+//        
+//        // Retrieve value
+//        SwiftyPlistManager.shared.getValue(for: key as! String, fromPlistWithName: dataPlistName) { (result, err) in
+//            if err == nil {
+//                guard let result = result else {
+//                    print("-------------> The Value for Key '\(key)' does not exists.")
+//                    return
+//                }
+//                print("-------------> The Value for Key '\(key)' actually exists. It is: '\(result)'")
+//            } else {
+//                print("No key in there!")
+//            }
+//        }
+//    }
 
     
     
